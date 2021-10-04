1f1ac142972da618019090c300741fb0f53e13c2
wnsvy <wnsvy223@naver.com>
2021-10-04 09:31:17 +0900

    어플리케이션 타임존 설정을 한국 시간으로 변경

--- src/main/java/security/ttaallkk/TtaallkkApplication.java
+++ src/main/java/security/ttaallkk/TtaallkkApplication.java
@@ -1,5 +1,8 @@
 package security.ttaallkk;
 
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -7,6 +10,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class TtaallkkApplication {
+
+	@PostConstruct
+	void timeZoneInit() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul")); // 타임존 설정(한국)
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TtaallkkApplication.class, args);

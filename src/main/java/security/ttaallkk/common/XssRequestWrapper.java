5e250d120b80102faec2de21fc3c82ea16a89856
wnsvy <wnsvy223@naver.com>
2021-11-25 20:25:42 +0900

    1. XSS필터 클래스에 Naver Lucy Filter 적용 2. XSS필터 클래스에 header, parameter, parameterValue 필터링 추가

--- src/main/java/security/ttaallkk/common/XssRequestWrapper.java
+++ src/main/java/security/ttaallkk/common/XssRequestWrapper.java
@@ -12,14 +12,18 @@ import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
 
+import com.nhncorp.lucy.security.xss.XssSaxFilter;
+import lombok.extern.log4j.Log4j2;
+
 /**
 * Request body로 들어오는 json 요청값에 대하여 XSS필터링 적용
 */
+@Log4j2
 public class XssRequestWrapper extends HttpServletRequestWrapper{
 
     private byte[] body;
 
-    public XssRequestWrapper(HttpServletRequest request) {
+    public XssRequestWrapper(HttpServletRequest request) throws IOException{
         super(request);
         try {
             String inputLine;
@@ -35,6 +39,34 @@ public class XssRequestWrapper extends HttpServletRequestWrapper{
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public String[] getParameterValues(String parameter) {
+        String[] values = super.getParameterValues(parameter);
+        if (values == null) {
+            return null;
+        }
+ 
+        int count = values.length;
+        String[] encodedValues = new String[count];
+        for (int i = 0; i < count; i++) {
+            encodedValues[i] = cleanXss(values[i]);
+        }
+ 
+        return encodedValues;
+    }
+ 
+    @Override
+    public String getParameter(String parameter) {
+        String value = super.getParameter(parameter);
+        return cleanXss(value);
+    }
+ 
+    @Override
+    public String getHeader(String name) {
+        String value = super.getHeader(name);
+        return cleanXss(value);
     }
 
     @Override
@@ -64,15 +96,20 @@ public class XssRequestWrapper extends HttpServletRequestWrapper{
         };
     }
 
-    // XSS필터링을 위한 escape 수행 함수
+    // XSS필터링 처리 함수 (feat. Naver Lucy Filter)
     private String cleanXss(String value) {
-        value = value.replaceAll("<", "&lt;").replaceAll(">", "&gt;");
-        value = value.replaceAll("\\(", "&#40;").replaceAll("\\)", "&#41;");
-        value = value.replaceAll("'", "&#39;");
-        value = value.replaceAll("&", "&amp;");
-        value = value.replaceAll("eval\\((.*)\\)", "");
-        value = value.replaceAll("[\\\"\\\'][\\s]*javascript:(.*)[\\\"\\\']", "\"\"");
-        value = value.replaceAll("script", "");
+        if(value != null) {
+            value = value.replaceAll("<", "&lt;").replaceAll(">", "&gt;");
+            value = value.replaceAll("\\(", "&#40;").replaceAll("\\)", "&#41;");
+            value = value.replaceAll("'", "&#39;");
+            value = value.replaceAll("&", "&amp;");
+            value = value.replaceAll("eval\\((.*)\\)", "");
+            value = value.replaceAll("[\\\"\\\'][\\s]*javascript:(.*)[\\\"\\\']", "\"\"");
+            
+            XssSaxFilter lucyFilter = XssSaxFilter.getInstance("lucy-xss-sax.xml", true);
+            value = lucyFilter.doFilter(value);
+            log.info(value);
+        }
         return value;
     }
 }
